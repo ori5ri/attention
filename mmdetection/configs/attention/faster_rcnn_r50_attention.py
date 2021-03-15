55a4cb00ed4282fadabb6ae15cf18fa9ccaf748d
famer9716 <famer3625@gmail.com>
2021-03-15 17:14:11 +0900

    add Detector faster_rcnn_r50_attention.py

---
+++ mmdetection/configs/attention/faster_rcnn_r50_attention.py
@@ -0,0 +1,12 @@
+_base_ = [
+    '../_base_/models/faster_rcnn_r50_fpn.py',
+    './_base_/dataset.py',
+    './_base_/schedule_2x.py', '../_base_/default_runtime.py'
+]
+model = dict(
+    neck=dict(
+        type='Attention',
+        in_channels=[256, 512, 1024, 2048],
+        out_channels=256,
+        num_outs=5)
+)
