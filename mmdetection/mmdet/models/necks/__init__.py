034d8c15426326f90ccd278945a27d265e06a2a8
famer9716 <famer3625@gmail.com>
2021-03-15 17:13:35 +0900

    add Neck attention.py

--- mmdetection/mmdet/models/necks/__init__.py
+++ mmdetection/mmdet/models/necks/__init__.py
@@ -9,8 +9,9 @@ from .nasfcos_fpn import NASFCOS_FPN
 from .pafpn import PAFPN
 from .rfp import RFP
 from .yolo_neck import YOLOV3Neck
+from .attention import Attention
 
 __all__ = [
     'FPN', 'BFP', 'ChannelMapper', 'HRFPN', 'NASFPN', 'FPN_CARAFE', 'PAFPN',
-    'NASFCOS_FPN', 'RFP', 'YOLOV3Neck', 'FPG'
+    'NASFCOS_FPN', 'RFP', 'YOLOV3Neck', 'FPG', 'Attention'
 ]
