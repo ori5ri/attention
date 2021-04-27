dbef08940a55b73d9552f1bf6a208edb093eb54f
famer9716 <famer3625@gmail.com>
2021-04-27 23:51:50 +0900

    Add _base_

--- mmdetection/configs/attention/_base_/dataset_640.py
+++ mmdetection/configs/attention/_base_/dataset_640.py
@@ -1,8 +1,7 @@
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
-
 data = dict(
-    samples_per_gpu=2,
+    samples_per_gpu=4,
     workers_per_gpu=2,
     train=dict(
         type='CocoDataset',
@@ -11,14 +10,14 @@ data = dict(
         pipeline=[
             dict(type='LoadImageFromFile'),
             dict(type='LoadAnnotations', with_bbox=True),
-            dict(type='Resize', img_scale=(1333, 800), keep_ratio=True),
+            dict(type='Resize', img_scale=(640, 640), keep_ratio=True),
             dict(type='RandomFlip', flip_ratio=0.5),
             dict(
                 type='Normalize',
                 mean=[123.675, 116.28, 103.53],
                 std=[58.395, 57.12, 57.375],
                 to_rgb=True),
-            dict(type='Pad', size_divisor=32),
+            dict(type='Pad', size=(640, 640)),
             dict(type='DefaultFormatBundle'),
             dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels'])
         ]),
@@ -30,7 +29,7 @@ data = dict(
             dict(type='LoadImageFromFile'),
             dict(
                 type='MultiScaleFlipAug',
-                img_scale=(1333, 800),
+                img_scale=(640, 640),
                 flip=False,
                 transforms=[
                     dict(type='Resize', keep_ratio=True),
@@ -40,7 +39,7 @@ data = dict(
                         mean=[123.675, 116.28, 103.53],
                         std=[58.395, 57.12, 57.375],
                         to_rgb=True),
-                    dict(type='Pad', size_divisor=32),
+                    dict(type='Pad', size=(640, 640)),
                     dict(type='ImageToTensor', keys=['img']),
                     dict(type='Collect', keys=['img'])
                 ])
@@ -53,7 +52,7 @@ data = dict(
             dict(type='LoadImageFromFile'),
             dict(
                 type='MultiScaleFlipAug',
-                img_scale=(1333, 800),
+                img_scale=(640, 640),
                 flip=False,
                 transforms=[
                     dict(type='Resize', keep_ratio=True),
@@ -63,7 +62,7 @@ data = dict(
                         mean=[123.675, 116.28, 103.53],
                         std=[58.395, 57.12, 57.375],
                         to_rgb=True),
-                    dict(type='Pad', size_divisor=32),
+                    dict(type='Pad', size=(640, 640)),
                     dict(type='ImageToTensor', keys=['img']),
                     dict(type='Collect', keys=['img'])
                 ])
