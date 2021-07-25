c709df9685065d3a2b124acf92b3eb697dae82f0
famer9716 <famer3625@gmail.com>
2021-07-25 18:43:41 +0900

    Add swin_transformer.py

--- mmdetection/mmdet/models/backbones/__init__.py
+++ mmdetection/mmdet/models/backbones/__init__.py
@@ -11,9 +11,10 @@ from .resnext import ResNeXt
 from .ssd_vgg import SSDVGG
 from .trident_resnet import TridentResNet
 from .mobilenet_v2 import MobileNetV2
+from .swin_transformer import SwinTransformer
 
 __all__ = [
     'RegNet', 'ResNet', 'ResNetV1d', 'ResNeXt', 'SSDVGG', 'HRNet', 'Res2Net',
     'HourglassNet', 'DetectoRS_ResNet', 'DetectoRS_ResNeXt', 'Darknet',
-    'ResNeSt', 'TridentResNet','MobileNetV2'
+    'ResNeSt', 'TridentResNet','MobileNetV2', 'SwinTransformer'
 ]
