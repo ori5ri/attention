cc799fa1a73c8540ecf815bbc63148934020f2b8
famer9716 <famer3625@gmail.com>
2021-07-25 18:09:49 +0900

    Add config

---
+++ mmdetection/configs/attention/faster_rcnn_r50_context_attention_add_repeated3_lateral_640_1x.py
@@ -0,0 +1,17 @@
+_base_ = [
+    '../_base_/models/faster_rcnn_r50_fpn.py',
+    './_base_/dataset_640.py',
+    './_base_/schedule_1x.py', '../_base_/default_runtime.py'
+]
+model = dict(
+    neck=dict(
+        type='Attention',
+        in_channels=[256, 512, 1024, 2048],
+        out_channels=256,
+        num_outs=5,
+        attention_type='context',
+        fusion_types=['channel_add'],
+        add_extra_convs='on_lateral',
+        repeated_layer=3
+    )
+)
